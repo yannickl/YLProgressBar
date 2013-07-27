5a41e8e1c7d88c7398045212450d668d6116e8a8
Yannick Loriot <yannick.loriot@gmail.com>
2013-07-27 13:10:04 +0200

    [NEW] Generate Doc

---
+++ appledoc_generate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+###
+# You need appledoc 2.1 (https://github.com/tomaz/appledoc/) to generate the documentation thanks to this script
+###
+
+mkdir documentation
+
+/Applications/appledoc -h AppledocSettings.plist ./YLProgressBar 
