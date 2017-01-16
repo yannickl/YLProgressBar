4482752fb3439662f2f62ab24523c1ad63a9519c
Yannick Loriot <yannick.loriot@gmail.com>
2017-01-16 23:03:15 +0100

    Fixing the #49

--- YLProgressBar/YLProgressBar.m
+++ YLProgressBar/YLProgressBar.m
@@ -615,14 +615,14 @@ const CGFloat YLProgressBarDefaultProgress = 0.3f;
     indicatorText = [NSString stringWithFormat:@"%.0f%%", (self.progress * 100)];
   }
 
-    CGRect textRect;
-    if ([indicatorText respondsToSelector:@selector(boundingRectWithSize:options:attributes:context:)])
-    {
-        textRect = [indicatorText boundingRectWithSize:CGRectInset(innerRect, 20, 0).size
-                                               options:NSStringDrawingUsesLineFragmentOrigin
-                                            attributes:@{ NSFontAttributeName: _indicatorTextLabel.font }
-                                               context:nil];
-    }
+  CGRect textRect = CGRectZero;
+  if ([indicatorText respondsToSelector:@selector(boundingRectWithSize:options:attributes:context:)])
+  {
+    textRect = [indicatorText boundingRectWithSize:CGRectInset(innerRect, 20, 0).size
+                                           options:NSStringDrawingUsesLineFragmentOrigin
+                                        attributes:@{ NSFontAttributeName: _indicatorTextLabel.font }
+                                           context:nil];
+  }
 
   if (innerRect.size.width < textRect.size.width || innerRect.size.height + 4 < textRect.size.height)
   {
@@ -686,7 +686,7 @@ const CGFloat YLProgressBarDefaultProgress = 0.3f;
       {
         [_stripesTimer invalidate];
       }
-
+      
       self.stripesTimer = nil;
     }
   }
